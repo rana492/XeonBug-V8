70f72b5a98034d36861cbc7885374d7125ee9ea5
rana492 <ranazeshan645@gmail.com>
2024-09-17 10:37:24 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "923157889544"
+global.ownername = "TANZEEM♥️"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
-global.botname = 'XeonBug-V8' //name of the bot
+global.ownernumber = '923157889544'  //creator number
+global.ownername = 'TANZEEM♥️' //owner name
+global.botname = '♥️' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
